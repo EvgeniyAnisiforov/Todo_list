27ff3c0d530ea5eed405a2fe0a50f8051ceaa749
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-08-01 22:54:38 +0300

    Подключение к БД

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, ()=>console.log(`Сервер запущен http://localhost:${PORT}`))
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, ()=>console.log(`Сервер запущен http://localhost:${PORT}`))
+    }catch(e){
+        console.log(e)
+    }
+}
+start()
